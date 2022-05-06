cba1e6005c4e3afc0c65fd0d2a94307a34ad97fb
Keenan Wilkinson <keenanwilkinson@outlook.com>
2022-05-06 12:27:38 -0500

    finalize oauth

--- project/spotipy/__init__.py
+++ project/spotipy/__init__.py
@@ -1,9 +1,3 @@
-# from .cache_handler import *  # noqa
-# from .client import *  # noqa
-# from .exceptions import *  # noqa
-# from .oauth2 import *  # noqa
-# from .util import *  # noqa
-
 from spotipy.errors import SpotifyException, SpotifyHttpError, \
     SpotifyStateError, SpotifyOAuthError
 
