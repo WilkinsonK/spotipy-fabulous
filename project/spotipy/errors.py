e9fd3eb364d1831a166631941b8b6c6783463437
Keenan Wilkinson <keenanwilkinson@outlook.com>
2022-07-17 21:46:47 -0500

    make protocols and typedefs top-level components.

--- project/spotipy/errors.py
+++ project/spotipy/errors.py
@@ -1,92 +1,77 @@
-from dataclasses import asdict, dataclass, field
-from typing import Any
+"""
+Listed below are various exceptions used for this
+package.
+"""
 
-
-@dataclass
-class ExceptionValues:
-    message: str
-
-
-@dataclass
-class HttpExceptionValues(ExceptionValues):
-    http_status: str
-    code:        int
-
-    # Optionals
-    reason:  str | None     = field(default=None)
-    headers: dict[str, Any] = field(default_factory=dict)
-
-
-@dataclass
-class StateExceptionValues(HttpExceptionValues):
-    local_state:  str | None = field(default=None)
-    remote_state: str | None = field(default=None)
+import http
 
 
 class SpotifyException(Exception):
     """
-    Raised in the event of any errors
-    related to the `Spotify API`.
+    Raised in the event of any errors related to
+    the `Spotify Web API`.
     """
-
-    template:   str                   = "{message}"
-    values_cls: type[ExceptionValues] = ExceptionValues
-
-    def __init__(self, message: str, *,
-        template: str = None,
-        values_cls: type[ExceptionValues] = None,
-        **kwargs):
-
-        if template:
-            self.template = template
-
-        if not values_cls:
-            values_cls = self.values_cls
-        self.values = values_cls(message, **kwargs)
-
-    def __str__(self):
-        return self.template.format(**asdict(self.values))
 
 
 class SpotifyHttpError(SpotifyException):
     """
     Raised in the event of any HTTP related
-    errors from the `Spotify API`.
+    errors from the `Spotify Web API`.
     """
 
-    template   = (
-        "[{code} {http_status}] '{message}': '{reason}'")
-    values_cls = HttpExceptionValues
+    status: http.HTTPStatus = http.HTTPStatus(500)
+    """Response code from `Spotify Web API`."""
+
+    def __init__(self, *values, status: None | http.HTTPStatus = None):
+        super().__init__(self, *values)
+
+        # Custom http attributes.
+        if status:
+            self.status = status
+
+    def __str__(self):
+        return "{}: {}".format(self.status.value, super().__str__())
 
 
-
-class SpotifyOAuthError(SpotifyHttpError):
+class SpotifyUnauthorizedError(SpotifyHttpError):
     """
-    Raised for errors during OAuth authentication.
-    """
-
-
-class SpotifyStateError(SpotifyHttpError):
-    """
-    Raised when the state sent differs from the
-    state recieved.
-    """
-
-    template   = (
-        SpotifyHttpError.template +
-        "\n\texpected {local_state} but recieved {remote_state}")
-    values_cls = StateExceptionValues
-
-
-class SpotifyModelError(SpotifyException):
-    """
-    Raised when there is a model related
-    error.
+    Raised in the event of some transaction is
+    attempted outside what the client is allowed.
     """
 
+    status: http.HTTPStatus = http.HTTPStatus(401)
 
-class SpotifyValidationError(SpotifyModelError):
+
+class SpotifyNotFoundError(SpotifyHttpError):
     """
-    Raised when validation fails on
-    some model.
+    Raised in the event of some transaction
+    fails because a resource could not be found.
+    """
+
+    status: http.HTTPStatus = http.HTTPStatus(404)
+
+
+class SpotifyInternalError(SpotifyHttpError):
+    """
+    Raised in the event the `Spotify Web API`
+    fails internally.
+    """
+
+    status: http.HTTPStatus = http.HTTPStatus(500)
+
+
+class SpotifyNotImplementedError(SpotifyHttpError):
+    """
+    Raised in the event of some transaction
+    failing because the target does not support
+    the request method.
+    """
+
+    status: http.HTTPStatus = http.HTTPStatus(501)
+
+
+class OAuth2Exception(Exception):
+    """
+    Raised in the event of any errors related to
+    `OAuth2.0` and authentication events.
     """
---
+++ project/spotipy/protocols.py
@@ -0,0 +1,62 @@
+import typing
+from abc import abstractmethod
+from typing import Protocol
+
+from spotipy import typedefs as td
+
+
+class RemoteAccessManager(Protocol):
+    """
+    Connects to some remote host or service.
+    """
+
+    @abstractmethod
+    def detach(self) -> td.ReturnState:
+        """Disconnects from the target."""
+
+    @abstractmethod
+    def attach(self) -> td.ReturnState:
+        """Connects to the target."""
+
+    @abstractmethod
+    def ping(self) -> td.ReturnState:
+        """
+        Sends a ping returning the state of the
+        target.
+        """
+
+
+class CacheManager(Protocol[td.GT_co]):
+    """
+    Brokers transactions of cached data.
+    Primarily for the use of relieving more
+    expensive transactions.
+    """
+
+    @abstractmethod
+    def find(self, key: str) -> None | td.GT_co:
+        """
+        Attempt to retrieve data from the cache
+        assigned to the given key. Returns `None`
+        if it fails.
+        """
+
+    @abstractmethod
+    def save(self, key: str, data: td.GT) -> td.GT_co:
+        """
+        Attempt to insert data into the cache,
+        assigning it to the given key.
+        """
+
+
+class OAuth2Flow(Protocol):
+    """
+    Represents an Authentication Flow procedure
+    defined by `OAuth2.0`. This object is
+    responsible for aquiring an authentication
+    token.
+    """
+
+    @abstractmethod
+    def aquire(self) -> td.CharToken:
+        """Attempt to retrieve and auth token."""
