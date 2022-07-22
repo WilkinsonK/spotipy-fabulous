ef553deab2354954042e0ff015ff4887c2075856
Keenan Wilkinson <keenanwilkinson@outlook.com>
2022-07-21 19:27:40 -0500

    update __init__.py

--- project/ampyr/__init__.py
+++ project/ampyr/__init__.py
@@ -3,8 +3,3 @@ Inspired by the existing package `spotipy`, ampyr
 implements its own flow for interacting with the
 `Spotify Web API`.
 """
-
-
-if __name__ == "__main__":
-    SPOTIFY_OAUTH_URL = "https://accounts.spotify.com/authorize"
-    SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
